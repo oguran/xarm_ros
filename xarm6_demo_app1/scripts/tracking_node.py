8285ee88b0f590fcdf094e167d8adf271fe8f92b
Takuya Ogura <takuya.ogura@sony.com>
2021-09-06 14:44:24 +0000

    fixed issue about conversion of position from rgb image to depth image

--- xarm6_demo_app1/scripts/tracking_node.py
+++ xarm6_demo_app1/scripts/tracking_node.py
@@ -5,6 +5,8 @@ import rospy
 import message_filters
 from std_msgs.msg import String
 from geometry_msgs.msg import Point
+from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import PoseStamped
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 
@@ -17,13 +19,13 @@ class testNode():
         self.mf = message_filters.ApproximateTimeSynchronizer([sub_rgb, sub_depth], 100, 0.5)
         self.mf.registerCallback(self.callback)
         # Publisherの作成
-        self.pub_target = rospy.Publisher('/camera/target', Point, queue_size=5)
+        self.pub_target = rospy.Publisher('/camera/target', PoseStamped, queue_size=5)
         self.pub_dbg_1 = rospy.Publisher('/camera/debug', Image, queue_size=5)
         self.pub_dbg_2 = rospy.Publisher('/camera/debug2', Image, queue_size=5)
 
     def callback(self, rgb_data, depth_data):
-        #print('rgb   x = {}, y = {}'.format(rgb_data.width, rgb_data.height))
-        #print('depth x = {}, y = {}'.format(depth_data.width, depth_data.height))
+        #print('rgb   width = {}, height = {}'.format(rgb_data.width, rgb_data.height))
+        #print('depth width = {}, height = {}'.format(depth_data.width, depth_data.height))
 
 
         try:
@@ -32,8 +34,8 @@ class testNode():
         except CvBridgeError, e:
             rospy.logerr(e)
 
-        gray_image = cv2.cvtColor(color_image , cv2.COLOR_BGR2GRAY)                  # グレースケール化
-        ret, img_binary = cv2.threshold(gray_image,                       # 二値化
+        gray_image = cv2.cvtColor(color_image , cv2.COLOR_BGR2GRAY)     # グレースケール化
+        ret, img_binary = cv2.threshold(gray_image,                     # 二値化
                 40, 255,                                                # 二値化のための閾値60(調整要)
                 cv2.THRESH_BINARY)
 
@@ -55,18 +57,22 @@ class testNode():
 
         img_circle = cv2.circle(color_image, (int(x), int(y)), 30, 155, 3)  # 検出した位置にサークル描画
 
-        send_img_circle = self.bridge.cv2_to_imgmsg(img_circle, rgb_data.encoding)                 # ROS msgに変換
-        x = x * depth_data.width / rgb_data.width
-        y = y * depth_data.height / rgb_data.height
+        send_img_circle = self.bridge.cv2_to_imgmsg(img_circle, rgb_data.encoding)  # ROS msgに変換
+        ofs = 320
+        x = x * float(depth_data.width - ofs) / float(rgb_data.width) + ofs/2
+        y = y * float(depth_data.height) / float(rgb_data.height)
 
         depth_image_4 = depth_image * 4
         d_img_circle = cv2.circle(depth_image_4, (int(x), int(y)), 30, 255*256, 3)  # 検出した位置にサークル描画
-        send_d_img = self.bridge.cv2_to_imgmsg(d_img_circle, depth_data.encoding)                 # ROS msgに変換
+        send_d_img = self.bridge.cv2_to_imgmsg(d_img_circle, depth_data.encoding)   # ROS msgに変換
         d_img_array = np.array(depth_image)
-        z_int16 = d_img_array[int(x), int(y)]
-        point = Point(x,y,float(z_int16))
+        z_int16 = d_img_array[int(y),int(x)]
+        ps = PoseStamped()
+        ps.header = depth_data.header
+        ps.pose.position = Point(y/1000,x/1000,float(z_int16)/1000)
+        ps.pose.orientation = Quaternion(0.0, 0.0, 0.0, 1.0)
 
-        self.pub_target.publish(point)
+        self.pub_target.publish(ps)
         self.pub_dbg_1.publish(send_img_circle)
         self.pub_dbg_2.publish(send_d_img)
 
