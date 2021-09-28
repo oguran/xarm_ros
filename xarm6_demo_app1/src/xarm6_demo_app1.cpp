cdadff52dd15e79768b764a28e09af27ea49e569
Takuya Ogura <takuya.ogura@sony.com>
2021-09-28 06:39:54 +0000

    Implemented cartesian velocity control for pre/post grasping

--- xarm6_demo_app1/src/xarm6_demo_app1.cpp
+++ xarm6_demo_app1/src/xarm6_demo_app1.cpp
@@ -161,6 +161,7 @@ class VisualServoTest {
         gripper_.waitForServer();
 
         pub_arm_cartesian_ = node_handle.advertise<geometry_msgs::PoseStamped>("/xarm/xarm6_cartesian_motion_controller/goal", 1);
+        pub_arm_cartesian_vel_ = node_handle.advertise<geometry_msgs::PoseStamped>("/xarm/xarm6_cartesian_motion_controller_velocity/goal", 1);
         pub_marker_= node_handle.advertise<visualization_msgs::Marker>("marker", 1);
         static message_filters::Subscriber<geometry_msgs::PoseStamped> sub_target(node_handle, "/xarm/camera/target", 1);
         static message_filters::Subscriber<sensor_msgs::Image> sub_image(node_handle, "/xarm/camera/depth/image", 1);
@@ -172,25 +173,17 @@ class VisualServoTest {
 
     bool MoveToCognitionPose(ros::NodeHandle& node_handle) {
       ROS_INFO("Moving to cognition pose");
-      geometry_msgs::PoseStamped pose;
-      pose.header.frame_id = FIXED_FRAME;
-      //pose.pose.position.x = 0.206873 - 0.50;
-      //pose.pose.position.y = 0.0;
-      //pose.pose.position.z = 0.111828 + 0.25;
-      pose.pose.position.x = -0.354;
-      pose.pose.position.y = -0.037;
-      pose.pose.position.z = 0.505;
+      cognition_pose_.header.frame_id = FIXED_FRAME;
+      cognition_pose_.pose.position.x = -0.354;
+      cognition_pose_.pose.position.y = -0.037;
+      cognition_pose_.pose.position.z = 0.505;
 
-#if 0
-      GetQuaternionMsg( 0.0, 3.14, 3.14, pose.pose.orientation);
-#else
-      pose.pose.orientation.x = 0.0;
-      pose.pose.orientation.y = 1.0;
-      pose.pose.orientation.z = 0.0;
-      pose.pose.orientation.w = 0.0;
-#endif
+      cognition_pose_.pose.orientation.x = 0.0;
+      cognition_pose_.pose.orientation.y = 1.0;
+      cognition_pose_.pose.orientation.z = 0.0;
+      cognition_pose_.pose.orientation.w = 0.0;
 
-      arm_.setPoseTarget(pose);
+      arm_.setPoseTarget(cognition_pose_);
       if (!arm_.move()) {
         ROS_WARN("Could not move to cognition pose");
         return false;
@@ -303,52 +296,6 @@ class VisualServoTest {
       return true;
     }
 
-    bool VisualServo(ros::NodeHandle& node_handle) {
-#if 0
-      std::vector<std::string> start_controller;
-      start_controller.push_back("xarm6_cartesian_motion_controller_velocity");
-      std::vector<std::string> stop_controller;
-      stop_controller.push_back("xarm6_traj_controller_velocity");
-
-      if (false == SwitchController(node_handle, start_controller, stop_controller)) {
-        return false;
-      }
-      ros::Duration(2).sleep();
-#endif
-
-      ROS_INFO("Moving to picking pose");
-      geometry_msgs::PoseStamped pose;
-      pose.header.frame_id = FIXED_FRAME;
-
-      ros::Rate rate(30);
-      while (ros::ok()) {
-        {
-          std::lock_guard<std::mutex> lock(mtx_);
-
-          if (pregrasped_) break;
-
-          pose.pose.position.x = target_pose_.position.x;
-          pose.pose.position.y = target_pose_.position.y;
-          pose.pose.position.z = target_pose_.position.z;
-
-          pose.pose.orientation.x = target_pose_.orientation.x;
-          pose.pose.orientation.y = target_pose_.orientation.y;
-          pose.pose.orientation.z = target_pose_.orientation.z;
-          pose.pose.orientation.w = target_pose_.orientation.w;
-        }
-        pub_arm_cartesian_.publish(pose);
-
-        rate.sleep();
-      }
-#if 1
-      pregrasped_= true;
-#endif
-
-      ROS_INFO("Moved to picking pose");
-
-      return true;
-    }
-
     bool Grasp(ros::NodeHandle& node_handle) {
 
       ROS_INFO("Start Grasp");
@@ -373,6 +320,334 @@ class VisualServoTest {
       pub_arm_cartesian_.publish(approaced_pose_);
 
       ROS_INFO("Moved to PostGrasped pose");
+
+      std::vector<std::string> start_controller;
+      start_controller.push_back("xarm6_traj_controller");
+      std::vector<std::string> stop_controller;
+      stop_controller.push_back("xarm6_cartesian_motion_controller");
+
+      ros::Duration(6).sleep();
+
+      if (false == SwitchController(node_handle, start_controller, stop_controller)) {
+        return false;
+      }
+
+      return true;
+    }
+
+    bool PostGraspVelocity(ros::NodeHandle& node_handle) {
+      ROS_INFO("Moving to PostGrasped pose");
+
+      static geometry_msgs::PoseStamped diff[2];
+      static geometry_msgs::PoseStamped integral;
+      memset(&diff[0], 0, sizeof(geometry_msgs::PoseStamped));
+      memset(&diff[1], 0, sizeof(geometry_msgs::PoseStamped));
+      memset(&integral, 0, sizeof(geometry_msgs::PoseStamped));
+
+      unsigned int count = 0;
+      ros::Rate rate(50);
+      while (ros::ok()) {
+        geometry_msgs::PoseStamped current_pose;;
+        geometry_msgs::TransformStamped approached_ts;
+        try { // link_tcpの現座標を取得
+          approached_ts = tfBuffer_.lookupTransform(FIXED_FRAME, "link_tcp", ros::Time(0), ros::Duration(1.0));
+        } catch (tf2::TransformException &ex) {
+          ROS_WARN("%s", ex.what());
+          return false;
+        }
+        transformTFStampedToPoseStamped(approached_ts, current_pose);
+
+        geometry_msgs::PoseStamped ref;
+        geometry_msgs::PoseStamped* p_target = &approaced_pose_;
+
+        // TODO
+        if (std::abs(p_target->pose.position.z - current_pose.pose.position.z) < 0.005) {
+          if (count > 50) {
+            ROS_WARN("!!!! braak : %lf", std::abs(p_target->pose.position.z - current_pose.pose.position.z));
+            break;
+          }
+          count++;
+        } else {
+          count--;
+        }
+
+        ref.header = p_target->header;
+
+        const float DELTA_T = 1.0/50.0;
+        const float KP = 400;
+        const float KP_R = 0;
+        const float KI = 10;
+        const float KI_R = 0;
+        const float KD = 3;
+        const float KD_R = 0;
+
+        diff[0].pose.position.x = diff[1].pose.position.x;
+        diff[0].pose.position.y = diff[1].pose.position.y;
+        diff[0].pose.position.z = diff[1].pose.position.z;
+        diff[0].pose.orientation.x = diff[1].pose.orientation.x;
+        diff[0].pose.orientation.y = diff[1].pose.orientation.y;
+        diff[0].pose.orientation.z = diff[1].pose.orientation.z;
+        diff[0].pose.orientation.w = diff[1].pose.orientation.w;
+        diff[1].pose.position.x = p_target->pose.position.x - current_pose.pose.position.x;
+        diff[1].pose.position.y = p_target->pose.position.y - current_pose.pose.position.y;
+        diff[1].pose.position.z = p_target->pose.position.z - current_pose.pose.position.z;
+        diff[1].pose.orientation.z = p_target->pose.orientation.x - current_pose.pose.orientation.x;
+        diff[1].pose.orientation.y = p_target->pose.orientation.y - current_pose.pose.orientation.y;
+        diff[1].pose.orientation.z = p_target->pose.orientation.z - current_pose.pose.orientation.z;
+        diff[1].pose.orientation.w = p_target->pose.orientation.w - current_pose.pose.orientation.w;
+
+        integral.pose.position.x += (diff[1].pose.position.x + diff[0].pose.position.x) / 2.0 * DELTA_T;
+        integral.pose.position.y += (diff[1].pose.position.y + diff[0].pose.position.y) / 2.0 * DELTA_T;
+        integral.pose.position.z += (diff[1].pose.position.z + diff[0].pose.position.z) / 2.0 * DELTA_T;
+        integral.pose.orientation.x += (diff[1].pose.orientation.x + diff[0].pose.orientation.x) / 2.0 * DELTA_T;
+        integral.pose.orientation.y += (diff[1].pose.orientation.y + diff[0].pose.orientation.y) / 2.0 * DELTA_T;
+        integral.pose.orientation.z += (diff[1].pose.orientation.z + diff[0].pose.orientation.z) / 2.0 * DELTA_T;
+        integral.pose.orientation.w += (diff[1].pose.orientation.w + diff[0].pose.orientation.w) / 2.0 * DELTA_T;
+
+        ref.pose.position.x
+          = p_target->pose.position.x
+          + KP * diff[1].pose.position.x
+          + KI * integral.pose.position.x
+          + KD * (diff[1].pose.position.x - diff[0].pose.position.x) / DELTA_T;
+        ref.pose.position.y
+          = p_target->pose.position.y
+          + KP * diff[1].pose.position.y
+          + KI * integral.pose.position.y
+          + KD * (diff[1].pose.position.y - diff[0].pose.position.y) / DELTA_T;
+        ref.pose.position.z
+          = p_target->pose.position.z
+          + KP * diff[1].pose.position.z
+          + KI * integral.pose.position.z
+          + KD * (diff[1].pose.position.z - diff[0].pose.position.z) / DELTA_T;
+        ref.pose.orientation.x
+          = p_target->pose.orientation.x
+          + KP_R * diff[1].pose.orientation.x
+          + KI_R * integral.pose.orientation.x
+          + KD_R * (diff[1].pose.orientation.x - diff[0].pose.orientation.x) / DELTA_T;
+        ref.pose.orientation.y
+          = p_target->pose.orientation.y
+          + KP_R * diff[1].pose.orientation.y
+          + KI_R * integral.pose.orientation.y
+          + KD_R * (diff[1].pose.orientation.y - diff[0].pose.orientation.y) / DELTA_T;
+        ref.pose.orientation.z
+          = p_target->pose.orientation.z
+          + KP_R * diff[1].pose.orientation.z
+          + KI_R * integral.pose.orientation.z
+          + KD_R * (diff[1].pose.orientation.z - diff[0].pose.orientation.z) / DELTA_T;
+        ref.pose.orientation.w
+          = p_target->pose.orientation.w
+          + KP_R * diff[1].pose.orientation.w
+          + KI_R * integral.pose.orientation.w
+          + KD_R * (diff[1].pose.orientation.w - diff[0].pose.orientation.w) / DELTA_T;
+
+        pub_arm_cartesian_.publish(ref);
+
+        rate.sleep();
+      }
+
+      ROS_INFO("Moved to PostGrasped pose");
+
+      std::vector<std::string> start_controller;
+      start_controller.push_back("xarm6_traj_controller_velocity");
+      std::vector<std::string> stop_controller;
+      stop_controller.push_back("xarm6_cartesian_motion_controller_velocity");
+
+      ros::Duration(6).sleep();
+
+      if (false == SwitchController(node_handle, start_controller, stop_controller)) {
+        return false;
+      }
+
+      return true;
+    }
+
+    bool PickVelocity(ros::NodeHandle& node_handle) {
+      std::vector<std::string> start_controller;
+      start_controller.push_back("xarm6_cartesian_motion_controller_velocity");
+      std::vector<std::string> stop_controller;
+      stop_controller.push_back("xarm6_traj_controller_velocity");
+
+      if (false == SwitchController(node_handle, start_controller, stop_controller)) {
+        return false;
+      }
+      //ros::Duration(2).sleep();
+
+      ROS_INFO("Moving to picking pose");
+      geometry_msgs::PoseStamped target_pose;
+      target_pose.header.frame_id = FIXED_FRAME;
+
+      {
+        std::lock_guard<std::mutex> lock(mtx_);
+
+        target_pose.pose.position.x = target_pose_.position.x + 0.01;
+        target_pose.pose.position.y = target_pose_.position.y;
+        //target_pose.pose.position.z = target_pose_.position.z - 0.07;
+        target_pose.pose.position.z = target_pose_.position.z - 0.06;
+
+        target_pose.pose.orientation.x = target_pose_.orientation.x;
+        target_pose.pose.orientation.y = target_pose_.orientation.y;
+        target_pose.pose.orientation.z = target_pose_.orientation.z;
+        target_pose.pose.orientation.w = target_pose_.orientation.w;
+      }
+
+      static geometry_msgs::PoseStamped diff[2];
+      static geometry_msgs::PoseStamped integral;
+      memset(&diff[0], 0, sizeof(geometry_msgs::PoseStamped));
+      memset(&diff[1], 0, sizeof(geometry_msgs::PoseStamped));
+      memset(&integral, 0, sizeof(geometry_msgs::PoseStamped));
+
+      geometry_msgs::PoseStamped* p_target = &target_pose;
+      geometry_msgs::PoseStamped ref;
+      geometry_msgs::PoseStamped current_pose;;
+      geometry_msgs::TransformStamped approached_ts;
+
+      unsigned int count = 0;
+      #define APPROACHED (0)
+      #define GRASPING (1)
+      #define GRASPED (2)
+      #define POSTGRASPED (3)
+      unsigned int status = APPROACHED;
+      bool isGrasped = false;
+      ros::Rate rate(50);
+      while (ros::ok()) {
+        try { // link_tcpの現座標を取得
+          approached_ts = tfBuffer_.lookupTransform(FIXED_FRAME, "link_tcp", ros::Time(0), ros::Duration(1.0));
+        } catch (tf2::TransformException &ex) {
+          ROS_WARN("%s", ex.what());
+          return false;
+        }
+        transformTFStampedToPoseStamped(approached_ts, current_pose);
+
+        float abs = 0.0f;
+        if ((abs = std::abs(p_target->pose.position.z - current_pose.pose.position.z)) < 0.005) {
+          switch(status) {
+          case APPROACHED:
+            if (count > 50) {
+              ROS_INFO("Start Grasp: abs = %f, target_z = %f, current_z = %f",
+                  abs, p_target->pose.position.z, current_pose.pose.position.z);
+              control_msgs::GripperCommandGoal goal;
+              goal.command.position = 0.5;
+              goal.command.max_effort = 10;
+              gripper_.sendGoal(goal);
+
+              status = GRASPING;
+              count = 0;
+            }
+            break;
+          case GRASPING:
+            if (count > 50) {
+              ROS_INFO("Grasped: abs = %f, target_z = %f, current_z = %f",
+                  abs, p_target->pose.position.z, current_pose.pose.position.z);
+              p_target = &approaced_pose_;
+              status = GRASPED;
+              count = 0;
+            }
+            break;
+          case GRASPED:
+            if (count > 100) {
+              ROS_INFO("Post Grasped: abs = %f, target_z = %f, current_z = %f",
+                  abs, p_target->pose.position.z, current_pose.pose.position.z);
+              ROS_INFO("break: abs = %f", abs);
+              status = POSTGRASPED;
+              break;
+            }
+          }
+
+          if (status == POSTGRASPED) {
+            break;
+          }
+
+          count++;
+        } else {
+          if (count > 0) count--;
+        }
+
+        ref.header = p_target->header;
+
+        const float DELTA_T = 1.0/50.0;
+        const float KP = 400;
+        const float KP_R = 0;
+        const float KI = 10;
+        const float KI_R = 0;
+        const float KD = 5;
+        const float KD_R = 0;
+
+        diff[0].pose.position.x = diff[1].pose.position.x;
+        diff[0].pose.position.y = diff[1].pose.position.y;
+        diff[0].pose.position.z = diff[1].pose.position.z;
+        diff[0].pose.orientation.x = diff[1].pose.orientation.x;
+        diff[0].pose.orientation.y = diff[1].pose.orientation.y;
+        diff[0].pose.orientation.z = diff[1].pose.orientation.z;
+        diff[0].pose.orientation.w = diff[1].pose.orientation.w;
+        diff[1].pose.position.x = p_target->pose.position.x - current_pose.pose.position.x;
+        diff[1].pose.position.y = p_target->pose.position.y - current_pose.pose.position.y;
+        diff[1].pose.position.z = p_target->pose.position.z - current_pose.pose.position.z;
+        diff[1].pose.orientation.z = p_target->pose.orientation.x - current_pose.pose.orientation.x;
+        diff[1].pose.orientation.y = p_target->pose.orientation.y - current_pose.pose.orientation.y;
+        diff[1].pose.orientation.z = p_target->pose.orientation.z - current_pose.pose.orientation.z;
+        diff[1].pose.orientation.w = p_target->pose.orientation.w - current_pose.pose.orientation.w;
+
+        integral.pose.position.x += (diff[1].pose.position.x + diff[0].pose.position.x) / 2.0 * DELTA_T;
+        integral.pose.position.y += (diff[1].pose.position.y + diff[0].pose.position.y) / 2.0 * DELTA_T;
+        integral.pose.position.z += (diff[1].pose.position.z + diff[0].pose.position.z) / 2.0 * DELTA_T;
+        integral.pose.orientation.x += (diff[1].pose.orientation.x + diff[0].pose.orientation.x) / 2.0 * DELTA_T;
+        integral.pose.orientation.y += (diff[1].pose.orientation.y + diff[0].pose.orientation.y) / 2.0 * DELTA_T;
+        integral.pose.orientation.z += (diff[1].pose.orientation.z + diff[0].pose.orientation.z) / 2.0 * DELTA_T;
+        integral.pose.orientation.w += (diff[1].pose.orientation.w + diff[0].pose.orientation.w) / 2.0 * DELTA_T;
+
+        ref.pose.position.x
+          = p_target->pose.position.x
+          + KP * diff[1].pose.position.x
+          + KI * integral.pose.position.x
+          + KD * (diff[1].pose.position.x - diff[0].pose.position.x) / DELTA_T;
+        ref.pose.position.y
+          = p_target->pose.position.y
+          + KP * diff[1].pose.position.y
+          + KI * integral.pose.position.y
+          + KD * (diff[1].pose.position.y - diff[0].pose.position.y) / DELTA_T;
+        ref.pose.position.z
+          = p_target->pose.position.z
+          + KP * diff[1].pose.position.z
+          + KI * integral.pose.position.z
+          + KD * (diff[1].pose.position.z - diff[0].pose.position.z) / DELTA_T;
+        ref.pose.orientation.x
+          = p_target->pose.orientation.x
+          + KP_R * diff[1].pose.orientation.x
+          + KI_R * integral.pose.orientation.x
+          + KD_R * (diff[1].pose.orientation.x - diff[0].pose.orientation.x) / DELTA_T;
+        ref.pose.orientation.y
+          = p_target->pose.orientation.y
+          + KP_R * diff[1].pose.orientation.y
+          + KI_R * integral.pose.orientation.y
+          + KD_R * (diff[1].pose.orientation.y - diff[0].pose.orientation.y) / DELTA_T;
+        ref.pose.orientation.z
+          = p_target->pose.orientation.z
+          + KP_R * diff[1].pose.orientation.z
+          + KI_R * integral.pose.orientation.z
+          + KD_R * (diff[1].pose.orientation.z - diff[0].pose.orientation.z) / DELTA_T;
+        ref.pose.orientation.w
+          = p_target->pose.orientation.w
+          + KP_R * diff[1].pose.orientation.w
+          + KI_R * integral.pose.orientation.w
+          + KD_R * (diff[1].pose.orientation.w - diff[0].pose.orientation.w) / DELTA_T;
+
+        pub_arm_cartesian_vel_.publish(ref);
+
+        rate.sleep();
+      }
+
+      ROS_INFO("Moved to picking pose");
+
+      start_controller.clear();
+      start_controller.push_back("xarm6_traj_controller_velocity");
+      stop_controller.clear();
+      stop_controller.push_back("xarm6_cartesian_motion_controller_velocity");
+
+      ros::Duration(6).sleep();
+
+      if (false == SwitchController(node_handle, start_controller, stop_controller)) {
+        return false;
+      }
 
       return true;
     }
@@ -448,8 +723,6 @@ void DepthTargetCallback(const geometry_msgs::PoseStampedConstPtr& msg_target,
 
         target_pose_.position = approaching_pose.pose.position;
         target_pose_.orientation = approaching_pose.pose.orientation;
-
-        //if (target_pose_.position.z) pregrasped_= true;
       }
     }
 
@@ -458,13 +731,14 @@ void DepthTargetCallback(const geometry_msgs::PoseStampedConstPtr& msg_target,
     moveit::planning_interface::PlanningSceneInterface scene_;
     actionlib::SimpleActionClient<control_msgs::GripperCommandAction> gripper_;
     ros::Publisher pub_arm_cartesian_;
+    ros::Publisher pub_arm_cartesian_vel_;
     ros::Publisher pub_marker_;
     const std::string PLANNING_GROUP = "xarm6";
     const std::string FIXED_FRAME = "world";
     geometry_msgs::Pose target_pose_;
+    geometry_msgs::PoseStamped cognition_pose_;
     geometry_msgs::PoseStamped approaced_pose_;
     std::mutex mtx_;
-    bool pregrasped_ = false;
     tf2_ros::Buffer tfBuffer_;
     tf2_ros::TransformListener tflistener_;
     image_geometry::PinholeCameraModel cam_model_;
@@ -481,11 +755,14 @@ int main(int argc, char** argv)
   VisualServoTest pnp(node_handle);
   pnp.MoveToCognitionPose(node_handle);
   pnp.DoApproach(node_handle);
+#if 0 // Position Control
   pnp.PreGrasp(node_handle);
   ros::Duration(5).sleep();
-  //pnp.VisualServo(node_handle);
   pnp.Grasp(node_handle);
   pnp.PostGrasp(node_handle);
+#else // Velocity Control
+  pnp.PickVelocity(node_handle);
+#endif
 
   //spinner.stop();
   // Wait until the node is shut down
